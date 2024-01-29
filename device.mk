a1538b3353c23615dc3e1bb80bd0ee92b835dd6a
lahaina <nattolecats@gmail.com>
2024-01-30 06:51:55 +0900

    denniz: configs: permissions: Add oplus privapp-permisssions java.lang.IllegalStateException: Signature|privileged permissions not in privapp-permissions allowlist: {com.oplus.exsystemservice (/system_ext/priv-app/OplusExSystemService): android.permission.SCHEDULE_EXACT_ALARM, com.oneplus.gallery (/system_ext/priv-app/OplusGallery): android.permission.SCHEDULE_EXACT_ALARM}

--- device.mk
+++ device.mk
@@ -311,6 +311,8 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml \
+    $(DEVICE_PATH)/configs/permissions/privapp-permissions-oplus.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-oplus.xml \
+    $(DEVICE_PATH)/configs/permissions/privapp-permissions-oplus.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-oplus.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
