bab2cc2583862b632f7650d0dace049ab7a7c0f7
lahaina <nattolecats@gmail.com>
2024-02-09 19:05:45 +0900

    denniz: logtag: Silence BatteryStatsHistory logspam

--- configs/props/logtag.mk
+++ configs/props/logtag.mk
@@ -234,4 +234,5 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.BufferQueueDump=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.BufferQueueProducer=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.SurfaceControl=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.GraphicBuffer=$(VENDOR_LOG_LEVEL)
+    persist.log.tag.GraphicBuffer=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.BatteryStatsHistory=$(VENDOR_LOG_LEVEL)
