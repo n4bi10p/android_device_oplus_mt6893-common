97bd5c9f21a90dd336ebcad48c68095eccbe93fa
lahaina <nattolecats@gmail.com>
2024-02-09 21:01:49 +0900

    denniz: vendorsetup: Fix clone derps

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,15 +11,16 @@ if [ -n "${CLEAN_DT_REPOS}" ]; then
         rm -rf hardware/lineage/compat
         rm -rf hardware/mediatek
         rm -rf hardware/oplus
+        rm -rf device/oneplus/denniz-kernel
         unset CLEAN_DT_REPOS
     fi
 fi
 echo start cloning repos
 VT=vendor/oneplus/denniz/denniz-vendor.mk
-if ! [ -a $VT ]; then git clone https://github.com/nishant6342/vendor_oneplus_denniz -b RMUI4-OSS vendor/oneplus/denniz
+if ! [ -a $VT ]; then git clone https://github.com/nattolecats/proprietary_vendor_oneplus_denniz -b udc vendor/oneplus/denniz
 fi
 KT=kernel/oneplus/denniz/Makefile
-if ! [ -a $KT ]; then git clone --depth=1 https://github.com/nishant6342/kernel_oneplus_denniz -b T kernel/oneplus/denniz
+if ! [ -a $KT ]; then git clone --depth=1 https://github.com/nattolecats/android_kernel_oneplus_denniz -b udc kernel/oneplus/denniz
 fi
 PA=packages/apps/prebuilt-apps/prebuilt-apps.mk
 if ! [ -a $PA ]; then git clone --depth=1 https://gitlab.com/nishant6342/packages_apps_prebuilt-apps packages/apps/prebuilt-apps/
@@ -34,7 +35,7 @@ POCKET=packages/apps/PocketMode/pocket_mode.mk
 if ! [ -a $POCKET ]; then git clone https://github.com/nishant6342/packages_apps_PocketMode packages/apps/PocketMode
 fi
 FW=vendor/oneplus/denniz-firmware/Android.mk
-if ! [ -a $FW ]; then git clone https://github.com/nishant6342/vendor_oneplus_denniz-firmware vendor/oneplus/denniz-firmware
+if ! [ -a $FW ]; then git clone https://github.com/nattolecats/android_vendor_oneplus_denniz-firmware vendor/oneplus/denniz-firmware
 fi
 CAM=device/oplus/camera/camera.mk
 if ! [ -a $CAM ]; then git clone --depth=1 https://gitlab.com/nishant6342/device_oplus_camera device/oplus/camera
@@ -46,12 +47,15 @@ MTK=hardware/mediatek/Android.bp
 if ! [ -a $MTK ]; then git clone https://github.com/nishant6342/android_hardware_mediatek -b lineage-20 hardware/mediatek
 fi
 CLANG17=prebuilts/clang/host/linux-x86/clang-r487747/bin/clang
-if ! [ -a $CLANG17 ]; then git clone https://gitlab.com/projectelixiros/android_prebuilts_clang_host_linux-x86_clang-r487747 -b Tiramisu prebuilts/clang/host/linux-x86/clang-r487747
+if ! [ -a $CLANG17 ]; then git clone https://gitlab.com/projectelixiros/android_prebuilts_clang_host_linux-x86_clang-r487747 prebuilts/clang/host/linux-x86/clang-r487747
 fi
 OPLUS=hardware/oplus/Android.mk
 if ! [ -a $OPLUS ]; then git clone https://github.com/nishant6342/android_hardware_oplus hardware/oplus
 fi
 WLAN=hardware/mediatek/wlan/Android.mk
 if ! [ -a $WLAN ]; then git clone https://github.com/nishant6342/android_hardware_mediatek_wlan hardware/mediatek/wlan
+fi
+KERNEL_PREBUILT=device/oneplus/denniz-kernel/dtbo.img
+if ! [ -a $KERNEL_PREBUILT ]; then git clone https://github.com/nattolecats/android_device_oneplus_denniz-kernel device/oneplus/denniz-kernel
 fi
 echo end cloning
