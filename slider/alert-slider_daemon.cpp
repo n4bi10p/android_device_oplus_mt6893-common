bba31358008bf7de3ce15e44907f46703f44ffa1
lahaina <nattolecats@gmail.com>
2024-02-25 11:37:03 +0900

    Revert "denniz: slider: Add haptic feedback on change position" * This breaks vibration strength.
    This reverts commit 2b5b211a6065cec23c33fe0dab7c5488090368dc.

--- slider/alert-slider_daemon.cpp
+++ slider/alert-slider_daemon.cpp
@@ -32,14 +32,6 @@ int read_tristate() {
     return atoi(p);
 }
 
-void vibrator_click() {
-    int fd = open("/sys/class/leds/vibrator/motor_old", O_WRONLY);
-    if (fd != -1) {
-        write(fd, "1", 1);
-        close(fd);
-    }
-}
-
 int main() {
     int fd = -1;
     for (int i = 0; i < 255; i++) {
@@ -72,6 +64,5 @@ int main() {
         } else if (state == 3) {
             system("service call audio 42 i32 2 s16 android");
         }
-        vibrator_click();
     }
 }
