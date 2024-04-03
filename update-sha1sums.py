0f2378fac3dc3c415db7b20276d598bb3dcda2e5
lahaina <nattolecats@gmail.com>
2024-04-03 18:13:52 +0900

    ossi: update-sha1sums: Adapt to mt6893-common

--- update-sha1sums.py
+++ update-sha1sums.py
@@ -20,8 +20,8 @@ import os
 import sys
 from hashlib import sha1
 
-device='X00T'
-vendor='asus'
+device='mt6893-common'
+vendor='oplus'
 
 with open('proprietary-files.txt', 'r') as f:
     lines = f.read().splitlines()
