4b98983d7f1b5b06837cb68ef257f483ef3db070
lahaina <nattolecats@gmail.com>
2024-04-13 18:05:54 +0900

    Revert "ossi: Cleanup dead targets" * Don't use Lineage common MTK HALs
    This reverts commit 310a02a.

--- ossi.mk
+++ ossi.mk
@@ -500,7 +500,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.mediatek
+    android.hardware.usb@1.3-service-mediatekv2
 
 # Vibrator
 PRODUCT_PACKAGES += \
