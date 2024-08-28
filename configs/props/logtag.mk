f9392579c9b852805b2eb9828ed238e2807378d9
n4bi10p <snabilshaikh186@gmail.com>
2024-08-28 21:18:02 +0530

    config: logtag: Silence gralloc4 logspam

--- configs/props/logtag.mk
+++ configs/props/logtag.mk
@@ -238,4 +238,5 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.BatteryStatsHistory=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.sensors-hal-OriginChannel=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.MDP=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.AISPQSelector=$(VENDOR_LOG_LEVEL)
+    persist.log.tag.AISPQSelector=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.gralloc4=$(VENDOR_LOG_LEVEL)
